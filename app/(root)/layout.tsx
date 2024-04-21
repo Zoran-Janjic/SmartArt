0eec65145d638fe1e1cada5dba63333c0c43cd1b
ZoranJanjic <zo-janjic@gl-navi.co.jp>
2024-04-21 21:35:16 +0900

    Add navbar and sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,12 @@
 import React from "react";
+import Sidebar from "@/components/shared/sidebar/Sidebar";
+import MobileNav from "@/components/shared/mobileNav/MobileNav";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
+      <Sidebar />
+      <MobileNav />
       <div className="root-container">
         <div className="wrapper">{children}</div>
       </div>
