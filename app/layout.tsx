9c9ce90455c7e2cb997b6c44c3df50a4627478b0
ZoranJanjic <zo-janjic@gl-navi.co.jp>
2024-04-21 17:38:34 +0900

    Completed Clerk user management

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const IBMPlex = IBM_Plex_Sans({
   subsets: ["latin"],
@@ -22,7 +23,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
-        {children}
+        <ClerkProvider appearance={{ variables: { colorPrimary: "#6459f0" } }}>
+          {children}
+        </ClerkProvider>
       </body>
     </html>
   );
